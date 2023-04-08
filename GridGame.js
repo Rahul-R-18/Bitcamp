c913d1af279515987ea827d1d08cd3982a6b5424
November8 <d.mcfawlt@gmail.com>
2023-04-08 11:58:56 -0400

    arrow movement

--- GridGame.js
+++ GridGame.js
@@ -1,31 +1,31 @@
 // JavaScript source code
 
-let gridSize = 5
-let grid = [[],[],[],[],[]]
-
+let gridSize = 4
+let grid = [[],[],[],[]]
+let player = null
 
 
 class Node {
-	var data;
-		
-	var up = null
-	var down = null
-	var left = null
-	var right = null
+	var data = 0;
+	
+	xPos = 0
+	yPos = 0 
+
 	//0-up 1-right 2-down 3-left
 	let directions = [null, null, null, null]
 	
-	constructor(data) {
+	constructor(data, x, y) {
 			
 		this.data = data;
-			
+		xPos = x
+		yPos = y
 	}
 }
 	
 
 
 class Path {
-	var type;
+	var type = 0;
 		
 	constructor(num) {
 		if (num == 0) {
@@ -45,9 +45,38 @@ class Player {
 		position = defaultPos
 	}
 
-	function move() {
-
+	function canMove(direction) {
+		return (position.directions[direction] != null)
 	}
+}
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        if (player.canMove(0)) {
+			player.position = grid[yPos + 1][xPos]
+		}
+    }
+    else if (e.keyCode == '40') {
+        if (player.canMove(2)) {
+			player.position = grid[yPos - 1][xPos]
+		}
+    }
+    else if (e.keyCode == '37') {
+		if (player.canMove(3)) {
+			player.position = grid[yPos][xPos - 1]
+		}
+    }
+    else if (e.keyCode == '39') {
+       if (player.canMove(1)) {
+			player.position = grid[yPos][xPos + 1]
+		}
+    }
+
 }
 
 function loadGrid() {
@@ -56,7 +85,7 @@ function loadGrid() {
 	//Initializing nodes
 	for (let i = 0; i < gridSize ; i++) {
 		for (let j = 0; j < gridSize ; j++) {
-			grid[i].push(new Node(Math.floor(Math.random() * 10)))
+			grid[i].push(new Node(Math.floor(Math.random() * 10), i, j))
 		}
 	}
 
@@ -75,9 +104,11 @@ function loadGrid() {
 			grid[j][i].directions[3] = grid[j][i].directions[1]
 		}
 	}
+
+	player = new Player(grid[0][0])
 }
 	
-function main(String[] args) {
+function main() {
 	loadGrid();
 }
 	
