affeec2a5e6fc843dbef1412fe5f3ed2be4fbc1e
Rahul-R-24 <83351013+Rahul-R-24@users.noreply.github.com>
2023-04-08 16:17:11 -0400

    Update GridGame.js

--- GridGame.js
+++ GridGame.js
@@ -17,7 +17,7 @@ let x = setInterval(function () {
 }, 1000)
 
 class Node {
-	data = 0;
+	data = 0
 	
 	xPos = 0
 	yPos = 0 
@@ -68,30 +68,34 @@ function checkKey(e) {
 
     if (e.keyCode == '38') {
         if (player.canMove(0)) {
+            countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[0])]);
             player.position = grid[yPos + 1][xPos]
             setTimeout(() => {  console.log("Moving Up"); }, 200);
-            countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[0])]);
+            countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
 		}
     }
     else if (e.keyCode == '40') {
         if (player.canMove(2)) {
+            countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[2])]);
             player.position = grid[yPos - 1][xPos]
             setTimeout(() => {  console.log("Moving Down"); }, 200);
-            countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[2])]);
+            countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
 		}
     }
     else if (e.keyCode == '37') {
 		if (player.canMove(3)) {
+		    countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[1])]);
 		    player.position = grid[yPos][xPos - 1]
 		    setTimeout(() => {  console.log("Moving Left"); }, 200);
-		    countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[1])]);
+		    countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
 		}
     }
     else if (e.keyCode == '39') {
        if (player.canMove(1)) {
+           countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[3])]);
            player.position = grid[yPos][xPos + 1]
            setTimeout(() => {  console.log("Moving Right"); }, 200);
-           countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[3])]);
+           countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
 		}
     }
 
@@ -135,5 +139,4 @@ function loadGrid() {
 }
 	
 loadGrid()
-
 
