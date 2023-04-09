8f8c67aacfd1e5e449b01a41260dcfd89f02c289
November8 <117789392+November8@users.noreply.github.com>
2023-04-09 01:40:03 -0400

    Update GridGame.js

--- GridGame.js
+++ GridGame.js
@@ -12,6 +12,9 @@ let yPosition = 0;
 let hasTimerStarted = false;
 let countdown = 30;
 
+let extraTime1 = [0,0]
+let extraTime2 = [0,0]
+
 document.addEventListener("keydown", function (event) {
 	if ((event.key === "A" || event.key === "a") && !hasTimerStarted) {
     	hasTimerStarted = true;
@@ -90,7 +93,7 @@ function checkKey(event) {
 	const speed = 315
 	switch (event.key) {
 		case "ArrowUp":
-
+			
 			if (player.canMove(0)) {
 				countdown -= dict[player.position.directions[0].type]
 				player.position = grid[player.position.yPos - 1][player.position.xPos]
@@ -139,10 +142,18 @@ function loadGrid() {
 	//Initializing nodes
 	for (let i = 0; i < gridSize ; i++) {
 		for (let j = 0; j < gridSize ; j++) {
-			grid[i].push(new Node(Math.floor(Math.random() * 10) + 1, j, i))
+			grid[i].push(new Node(0, j, i))
 		}
 	}
 
+	randomizeGrid()
+
+	player = new Player(grid[0][0])
+
+	console.log(grid)
+}
+
+function randomizeGrid() {
 	counter = 0
 	layerCounter = 1
 	//Initializing vertical paths
@@ -182,9 +193,21 @@ function loadGrid() {
 		layerCounter += 1
 	}
 
-	player = new Player(grid[0][0])
 
-	console.log(grid)
+	//Choose extraTimes
+	extraTime1 = [Math.floor(Math.random() * gridSize), Math.floor(Math.random() * gridSize)]
+	grid[extraTime1[0]][extraTime1[1]].data = Math.floor(Math.random() * 6) + 5
+	extraTime2 = [Math.floor(Math.random() * gridSize), Math.floor(Math.random() * gridSize)]
+	grid[extraTime2[0]][extraTime2[1]].data = Math.floor(Math.random() * 6) + 5
+
+	//setPositions of extraTimes
+	document.getElementById("textbox1").style.top = (extraTime1[0] * 315) + "px"
+	document.getElementById("textbox1").style.left = (extraTime1[1] * 315) + "px"
+	document.getElementById("textbox1").innerHTML = "<p>" + grid[extraTime1[0]][extraTime1[1]].data + "</p>"
+
+	document.getElementById("textbox2").style.top = (extraTime2[0] * 315) + "px"
+	document.getElementById("textbox2").style.left = (extraTime2[1] * 315) + "px"
+	document.getElementById("textbox2").innerHTML = "<p>" + grid[extraTime2[0]][extraTime2[1]].data + "</p>"
 }
 
 console.log(document.querySelector(".border.layer4.rec09").style)
