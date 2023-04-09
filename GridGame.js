2e6ffbf92db0a1163b2197ecb40e8bef2dbce3ae
November8 <117789392+November8@users.noreply.github.com>
2023-04-09 00:30:22 -0400

    Update GridGame.js

--- GridGame.js
+++ GridGame.js
@@ -131,21 +131,6 @@ function checkKey(event) {
 
 }
 
-document.addEventListener("keydown", function (event) {
-	if ((event.key === "A" || event.key === "a") && !hasTimerStarted) {
-		countdown = 30;
-		hasTimerStarted = true;
-		const interval = setInterval(function () {
-			countdown--;
-			document.getElementById("countdown").innerHTML = "00:" + countdown;
-			if (countdown <= 0) {
-				clearInterval(interval);
-				document.getElementById("timer").innerHTML = "Time's Up!";
-			}
-		}, 1000);
-	}
-});
-
 function loadGrid() {
 
 
