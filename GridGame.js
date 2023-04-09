20d1c2c2884b7bc2e73f93a4c7be8f123fef11f8
November8 <117789392+November8@users.noreply.github.com>
2023-04-08 21:51:26 -0400

    Update GridGame.js

--- GridGame.js
+++ GridGame.js
@@ -5,10 +5,13 @@ let grid = [[],[],[],[]]
 let player = null
 let dict = {"mud":2,"ice":0,"reg":1}
 let color_dict = {"mud":"brown","ice":"lightblue","reg":"grey"}
-let inverse_dict = {"left":"right","right":"left","up":"down","down","up"}
-const colors = ['brown', 'lightblue', 'grey'];
+let inverse_dict = {"left":"right","right":"left","up":"down","down":"up"}
 let countDownDate = new Date()
 countDownDate.setSeconds(countDownDate.getSeconds() + 60)
+old_position = null
+
+let xPosition = 0;
+let yPosition = 0;
 
 const grayBorders = document.querySelectorAll('.border.layer1, .border.layer2, .border.layer3, .border.layer4');
 
@@ -36,6 +39,10 @@ class Node {
 		this.yPos = y
 		
 	}
+
+	getData() {
+		return this.data
+	}
 }
 	
 
@@ -58,64 +65,79 @@ class Path {
 }
 
 class Player {
-	position = new Node(5,0,0)
+	position = null
 	dummy_pos = 0
+
 	constructor(defaultPos) {
 		this.position = defaultPos
 	}
 
 	canMove(direction) {
-		return (position.directions[direction] != null && direction != inverse_dict[dummy_pos])
+		old_position = this.position
+		return (this.position.directions[direction] != null && direction != inverse_dict[this.dummy_pos])
 	}
 }
 
-document.onkeydown = checkKey;
+window.addEventListener("keydown", checkKey);
 
-function checkKey(e) {
+function checkKey(event) {
+	const speed = 325
+	switch (event.key) {
+		case "ArrowUp":
+			
+			if (player.canMove(0)) {
 
-    e = e || window.event;
-
-    if (e.keyCode == '38') {
-        if (player.canMove(0)) {
-            countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[0])]);
-            player.position = grid[yPos + 1][xPos]
-            setTimeout(() => {  console.log("Moving Up"); }, 200);
-            countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
-            old_position.data=(Math.random() * 10)+1
-	    dummy_pos = "up"
-		}
-    }
-    else if (e.keyCode == '40') {
-        if (player.canMove(2)) {
-            old_position = player.position
-            countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[2])]);
-            player.position = grid[yPos - 1][xPos]
-            setTimeout(() => {  console.log("Moving Down"); }, 200);
-            countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
-            old_position.data=(Math.random() * 10)+1
-	    dummy_pos = "down"
-		}
-    }
-    else if (e.keyCode == '37') {
-		if (player.canMove(3)) {
-		    countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[1])]);
-		    player.position = grid[yPos][xPos - 1]
-		    setTimeout(() => {  console.log("Moving Left"); }, 200);
-		    countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
-		    old_position.data=(Math.random() * 10)+1
-		    dummy_pos = "left"
-		}
-    }
-    else if (e.keyCode == '39') {
-       if (player.canMove(1)) {
-           countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[3])]);
-           player.position = grid[yPos][xPos + 1]
-           setTimeout(() => {  console.log("Moving Right"); }, 200);
-           countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
-           old_position.data=(Math.random() * 10)+1
-	   dummy_pos = "right"
-		}
-    }
+				countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[0])]);
+				player.position = grid[player.position.yPos + 1][player.position.xPos]
+				setTimeout(() => {  console.log("Moving Up"); }, 200);
+				countDownDate.setSeconds(countDownDate.getSeconds()+ player.position.data)
+				old_position.data=(Math.random() * 10)+1
+				yPosition -= speed;
+				dummy_pos = "up"
+			}
+			break;
+		case "ArrowDown":
+			if (player.canMove(2)) {
+				old_position = player.position
+				countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[2])]);
+				player.position = grid[player.position.yPos - 1][player.position.xPos]
+				setTimeout(() => {  console.log("Moving Down"); }, 200);
+				countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
+				old_position.data=(Math.random() * 10)+1
+				yPosition += speed;
+				dummy_pos = "down"
+			}
+			
+			break;
+		case "ArrowLeft":
+			if (player.canMove(3)) {
+				countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[1])]);
+				player.position = grid[player.position.yPos][player.position.xPos - 1]
+				setTimeout(() => {  console.log("Moving Left"); }, 200);
+				countDownDate.setSeconds(countDownDate.getSeconds()+ player.position.data)
+				old_position.data=(Math.random() * 10)+1
+				xPosition -= speed;
+				dummy_pos = "left"
+			}
+			
+			break;
+		case "ArrowRight":
+			if (player.canMove(1)) {
+				countDownDate.setSeconds(countDownDate.getSeconds() - dict[(player.position.directions[3])]);
+				player.position = grid[player.position.yPos][player.position.xPos + 1]
+				setTimeout(() => {  console.log("Moving Right"); }, 200);
+				countDownDate.setSeconds(countDownDate.getSeconds()+player.position.data)
+				old_position.data=(Math.random() * 10)+1
+				xPosition += speed;
+				dummy_pos = "right"
+			 }
+			
+			break;
+		default:
+			break;
+	}
+	sprite.style.top = yPosition + "px";
+	sprite.style.left = xPosition + "px";
 
 } 
 
@@ -125,7 +147,7 @@ function loadGrid() {
 	//Initializing nodes
 	for (let i = 0; i < gridSize ; i++) {
 		for (let j = 0; j < gridSize ; j++) {
-			grid[i].push(new Node(Math.floor(Math.random() * 10)+1, i, j))
+			grid[i].push(new Node(Math.floor(Math.random() * 10) + 1, i, j))
 		}
 	}
 	
@@ -170,7 +192,7 @@ function loadGrid() {
 			grid[i][j + 1].directions[3] = grid[i][j].directions[1]
 
 			document.querySelector(grid[i][j].directions[1].elementName).style.backgroundColor = 
-				color_dict[grid[i][j].directions[2].type]
+				color_dict[grid[i][j].directions[1].type]
 		}
 
 		layerCounter += 1
@@ -179,4 +201,5 @@ function loadGrid() {
 	player = new Player(grid[0][0])
 }
 
+console.log(document.querySelector(".border.layer4.rec09").style)
 loadGrid()
