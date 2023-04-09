ad6db0164a74cc02f8a5f7d8e607e2b2d85495fc
Scott Chen <96890985+skottchen@users.noreply.github.com>
2023-04-08 23:37:51 -0400

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,12 +1,17 @@
 hasTimerStarted = false;
-
     document.addEventListener("keydown", function (event) {
         if ((event.key === "A" || event.key === "a") && !hasTimerStarted) {
             let countdown = 30;
             hasTimerStarted = true;
             const interval = setInterval(function () {
                 countdown--;
-                document.getElementById("countdown").innerHTML = "00:" + countdown;
+
+                if(countdown<10){
+                    document.getElementById("countdown").innerHTML = "00:0" + countdown;
+                } else {
+                    document.getElementById("countdown").innerHTML = "00:" + countdown;
+                }
+                
                 if (countdown <= 0) {
                     clearInterval(interval);
                     document.getElementById("timer").innerHTML = "Time's Up!";
